577ceecd17e52206d3d268d9b1d24faa9a0d8672
Novikova Tatiana <tania.novikov@web.de>
2024-01-29 17:05:42 +0100

    Library with PROMPT

---
+++ script.js
@@ -0,0 +1,46 @@
+const library = [];
+//isbn;title;author;year
+let inputData = prompt('Enter book data separate by ";"');
+while (inputData) {
+    //create and add to library only unique book by isbn
+    const arr = inputData.split(';');//превращает стринг в массив
+    if (findBook(library, arr[0]) === -1) {
+        const book = new Book(arr[0], arr[1], arr[2], arr[3]);
+        library.push(book);
+    }
+    inputData = prompt('Enter book data separate by ";"');
+}
+
+printLibrary(library);
+
+function printLibrary(library) {
+    //print all books to console
+    for (let index = 0; index < library.length; index++) {
+        console.log(library[index].toString());
+    }
+}
+
+function findBook(library, isbn) {
+    //return index if book exists, otherwise return -1
+    for (let i = 0; i < library.length; i++) {
+        if (library[i].isbn === isbn) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
+
+function Book(isbn, title, author, year) {
+    this.isbn = isbn;
+    this.title = title;
+    this.author = author;
+    this.year = +year;
+    this.toString = function () {
+        return `ISBN: ${this.isbn}, Title: ${this.title}, Author: ${this.author}, Year of publishing: ${this.year}`
+    }
+}
+
+
